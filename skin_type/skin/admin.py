86b221c4e81ed88d16fa667c3a5fc77bebe891bc
Hyuna Shin <hyuna1615@gmail.com>
2017-12-06 04:50:59 +0900

    comment line updated in q30

--- skin_type/skin/admin.py
+++ skin_type/skin/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import UserName, Comment
+from .models import Comment
 
 #admin page로 데이터모델 조작하기
 # Register your models here.
 #
 # from .models import
-admin.site.register(UserName)
+# admin.site.register(Post)
 admin.site.register(Comment)
